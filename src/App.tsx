6f0687053f78683463d7eb8f2991e1b4099e0aa2
Hongbin Li <hblee12294@gmail.com>
2019-02-14 15:37:35 -0800

    feat: init settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss'
+import logo from './logo.svg'
 
 class App extends Component {
-  render() {
+  public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="app">
+        <header className="app-header">
+          <img src={ logo } className="app-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className="app-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -21,8 +21,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
